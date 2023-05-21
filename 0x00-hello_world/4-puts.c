d7a7bb26b88be9a9deb857af8116a158f6e4955b
Chrizyawe <ceesuper6@gmail.com>
2023-05-21 11:44:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry point 0 success
+ *
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle"/n;
+
+	puts(str); /* writing the string to stdout */
+
+	Return 0;
+}
