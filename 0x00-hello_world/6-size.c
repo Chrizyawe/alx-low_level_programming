de3805ace80a8bd4f916c41e4e83278c266ffc62
Chrizyawe <ceesuper6@gmail.com>
2023-05-21 13:58:25 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	printf("size of int : %d", sizeof(int));
-	printf("size of float : %f", sizeof(float));
+	printf("size of float : %d", sizeof(float));
 	printf("size of char : %d", sizeof(char));
 	printf("size of long int : %d", sizeof(long int));
 	printf("size of long long int : %d", sizeof(long long int));
