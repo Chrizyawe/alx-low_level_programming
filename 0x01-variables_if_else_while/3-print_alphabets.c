3904ae89d1e57e392b44e774c3c4b714f52cdc08
Chrizyawe <ceesuper6@gmail.com>
2023-05-24 08:46:30 -0700

    0-putchar.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercase and uppercase 
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char alp[52]="abcdefghijklmnopqrstuvwxyzABECDFGHIJKLMNOPQRSTUVWXYZ";
+		int b;
+
+		for (b = 0; b < 52; b++)
+		{
+			putchar(alp[52]);
+		}
+		putchar('\n');
+		return (0);
+}
