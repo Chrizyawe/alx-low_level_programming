8a5c93459ed9f8b988cef8bf31aabef6ce4e718e
Chrizyawe <ceesuper6@gmail.com>
2023-05-28 03:46:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing base number
+ *
+ * Return: Always (success) 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
