0024bf4eb1cf9e05cf07f6626ca6e0a601277bb3
Chrizyawe <ceesuper6@gmail.com>
2023-06-13 08:20:25 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - printes a program name
+ * @argc: number of arguement
+ * @argv: array of aguement
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("program name: %s\n", argv[0]);
+	return (0);
+}
