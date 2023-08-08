1cf682fb0bc666089d890ebb69d766eeab744dca
Chrizyawe <supercee6@gmail.com>
2023-08-08 07:25:25 -0700

    alx project file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -35,12 +35,12 @@ void display_elf_header(Elf32_Ehdr *ehdr)
 		printf("%02x ", ehdr->e_ident[i]);
 	}
 	printf("\n");
-	printf("  Class:                             %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", ehdr->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
-	printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n", (ehdr->e_type == ET_EXEC) ? "EXEC (Executable file)" :
+	printf("  Class: %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:  %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:%d (current)\n", ehdr->e_ident[EI_VERSION]);
+	printf("  OS/ABI:%s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+	printf("  ABI Version: %d\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("  Type: %s\n", (ehdr->e_type == ET_EXEC) ? "EXEC (Executable file)" :
 			(ehdr->e_type == ET_DYN) ? "DYN (Shared object file)" :
 			(ehdr->e_type == ET_REL) ? "REL (Relocatable file)" : "Other");
 	printf("  Entry point address:               %#x\n", ehdr->e_entry);
